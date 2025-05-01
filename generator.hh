b956883e79373413fd1855f5c92a9523df259793
laggenerator <tomaszgasior30+github@gmail.com>
2025-05-01 14:25:00 +0200

    matfiz cos zrobil... porzadki

--- generator.hh
+++
@@ -1,9 +0,0 @@
-#ifndef GENERATOR_HH
-#define GENERATOR_HH
-
-#include "pair.hh"
-#include <random>
-
-void generujDane(Pair *tablica, size_t rozmiar, int seed, int minASCII, int maxASCII);
-
-#endif
